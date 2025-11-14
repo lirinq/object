971a385f9c02be7b30550d9c49d2a54d2f3ff055
lirinq <gaoqiaolinq@gmail.com>
2025-11-14 15:25:47 +0900

    2025/11/14 全体の整理　言語化を始めた　とりあえずではじめしすぎてごちゃごちゃなので整理を頑張りたい

--- enemy.js
+++ enemy.js
@@ -11,7 +11,7 @@ class  chara {
     }
     
     damage(dmgtaken){
-        this.hp -= damtaken;
+        this.hp -= dmgtaken;
         if(this.hp <= 0){
             this.hp = 0;
             console.log(this.name + 'は倒れた！');
@@ -19,20 +19,22 @@ class  chara {
     }
 
     heal(healtaken){
-        this.hp += healtaken;
-        if(this.hp <= this.maxHp){
-        this.hp = this.hp + healtaken;
-        this.hp = this.maxHp;
-        }else{
-        this.hp = healtaken + this.hp;
-        console.log('すでにHPは満タンだ！');
+        this.hp += healtaken;//healtakenに代入された数値をthis.hp(キャラクターのHP)に足す
+        
+        //代入された値によって変わる内容
+        if(this.hp <= this.maxHp){//もしこのキャラのHPがMAXHP以下なら
+                console.log('HPを' + healtaken + "回復した");
+
+        }else{//それ以外、つまりHPが満タンかそれ以上の場合
+            console.log('すでにHPは満タンだ！');
+            this.hp = this.maxHp;//最大HPを超えて回復しないように最大HPと現在のHPを同じにする
         }
     }
-    castSpell(caster, target){
-        if(caster.mp < this.cost){
-            caster.mp -= this.mp;
-            return effect;
-        }else if(target.mp > this.cost){
+    castSpell(caster, target,cast){//魔法を唱えるためのプロパティ　(詠唱者、標的、唱える魔法)
+        if(caster.mp < cast.cost){
+            caster.mp -= cast.mp;
+            return cast.effect;
+        }else if(target.mp > cast.cost){
             console.log('MPがたりない！');
         }
     }
@@ -42,7 +44,7 @@ class  chara {
 //             ～ここから攻撃に関するクラス～
 // ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
-class magic {
+class magic {//魔法を作るための型
     constructor(name,cost,effect) {
         this.name = name;//魔法の名前
         this.cost = cost;//消費MP
@@ -52,15 +54,15 @@ class magic {
 }
 
 
-function heal(){
-    taraget.hp -= 4;    
+function miniHeal(taraget){
+    taraget.hp += 4;    
 }
 
 
-let healing = new magic( hoimi,4,heal());
+let healing = new magic( "hoimi",4,miniHeal(taraget));
 const slarin = new chara("スラりん","スライムベス",9,38,12,12);
 const slami = new chara("スラみ","スライムベス",14,12,12,12);
 
-slami.castSpell(slami,slami).healing;
+slami.castSpell(slami,slami,healing);
 console.log(slami);
 
